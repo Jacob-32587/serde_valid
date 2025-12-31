543e7f3955280995c3e5e86e0990e031ec9378e9
Jacob-32587 <jgbs32587@outlook.com>
2025-12-30 21:58:26 -0500

    Ported over a string literal specific version of ser de attribute parsing and base case hanlding from serde library
    Created modules with functions that can parse and case convert given
    serde container rename attributes.

--- crates/serde_valid_derive/src/serde.rs
+++ crates/serde_valid_derive/src/serde.rs
@@ -1,1 +1,3 @@
+pub mod attr;
+pub mod case;
 pub mod rename;
---
+++ crates/serde_valid_derive/src/serde/attr.rs
@@ -0,0 +1,52 @@
+fn get_ser_and_de_rename(
+    attr_name: &str,
+    meta: &syn::meta::ParseNestedMeta,
+) -> syn::Result<SerAndDe> {
+    let mut ser_meta = None;
+    let mut de_meta = None;
+
+    let lookahead = meta.input.lookahead1();
+    if lookahead.peek(syn::Token![=]) {
+        if let Some(both) = get_lit_str2(meta)?.map(|x| x.value()) {
+            ser_meta = Some(both.clone());
+            de_meta = Some(both);
+        }
+    } else if lookahead.peek(syn::token::Paren) {
+        meta.parse_nested_meta(|meta| {
+            if meta.path.is_ident("serialize") {
+                ser_meta = get_lit_str2(&meta)?.map(|x| x.value());
+            } else if meta.path.is_ident("deserialize") {
+                de_meta = get_lit_str2(&meta)?.map(|x| x.value());
+            } else {
+                return Err(meta.error(format_args!(
+                    "malformed {0} attribute, expected `{0}(serialize = ..., deserialize = ...)`",
+                    attr_name,
+                )));
+            }
+            Ok(())
+        })?;
+    } else {
+        return Err(lookahead.error());
+    }
+
+    Ok((ser_meta, de_meta))
+}
+
+type SerAndDe = (Option<String>, Option<String>);
+
+fn get_lit_str2(meta: &syn::meta::ParseNestedMeta) -> syn::Result<Option<syn::LitStr>> {
+    let expr: syn::Expr = meta.value()?.parse()?;
+    let mut value = &expr;
+    while let syn::Expr::Group(e) = value {
+        value = &e.expr;
+    }
+    if let syn::Expr::Lit(syn::ExprLit {
+        lit: syn::Lit::Str(lit),
+        ..
+    }) = value
+    {
+        Ok(Some(lit.clone()))
+    } else {
+        Ok(None)
+    }
+}
